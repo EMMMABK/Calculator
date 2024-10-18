58c1acceeca80c558358763b2859c3aa9cfe515d
EMMMABK <adilbikiev2@gmail.com>
2024-10-18 23:49:09 +0600

    add 7th commit

--- src/main/java/org/example/calculator/HelloController.java
+++ src/main/java/org/example/calculator/HelloController.java
@@ -1,11 +1,15 @@
 package org.example.calculator;
 
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
     Calculator calc = new Calculator();
+
+    @FXML
+    private Label label;
 
     @FXML
     private TextField text;
@@ -17,60 +21,70 @@ public class HelloController {
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton2(){
         String val = text.getText();
         val = val + "2";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton3(){
         String val = text.getText();
         val = val + "3";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton4(){
         String val = text.getText();
         val = val + "4";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton5(){
         String val = text.getText();
         val = val + "5";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton6(){
         String val = text.getText();
         val = val + "6";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton7(){
         String val = text.getText();
         val = val + "7";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton8(){
         String val = text.getText();
         val = val + "8";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton9(){
         String val = text.getText();
         val = val + "9";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButton0(){
         String val = text.getText();
         val = val + "0";
         text.setText(val);
     }
 
+    @FXML
     private void onClickButtonPoint(){
         String val = text.getText();
         val = val + ".";
