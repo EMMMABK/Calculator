0863341fe0566c5d9ac7e5f756660f9e03541ed0
EMMMABK <adilbikiev2@gmail.com>
2024-10-18 23:29:51 +0600

    add 5th commit

--- src/main/java/org/example/calculator/Calculator.java
+++ src/main/java/org/example/calculator/Calculator.java
@@ -1,12 +1,18 @@
 package org.example.calculator;
 
+import javafx.animation.FadeTransition;
+import javafx.scene.control.TextField;
+import javax.swing.*;
+
 public class Calculator {
     private double op1;
     private double op2;
     private char operator;
     private double result;
+    private boolean isError ;
 
     public void calculate(){
+        isError = false;
         if(this.operator == '+'){
             this.result = this.op1 + this.op2;
         } else if (this.operator == '-') {
@@ -14,7 +20,13 @@ public class Calculator {
         } else if (this.operator == '*') {
             this.result = this.op1 * this.op2;
         } else if (this.operator == '/') {
-            this.result = this.op1 / this.op2;
+            if(this.op2==0){
+                isError=true;
+                System.out.println("Error: Division by zero.");
+                this.result = 0;
+            }else{
+                this.result = this.op1 / this.op2;
+            }
         }
     }
 
@@ -48,5 +60,17 @@ public class Calculator {
 
     public void setResult(double result){
         this.result = result;
+    }
+
+    public void reset(){
+        this.op1 = 0;
+        this.op2 = 0;
+        this.operator = ' ';
+        this.result = 0;
+        this.isError = false;
+    }
+
+    public boolean isError(){
+        return  isError;
     }
 }
